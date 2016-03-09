1a503d62403610e87c3f9b1edf4143b10588cb98
William L. Marr III <hobbyist@wlmiii.net>
2016-03-09 09:27:32 -0500

    Add Player subclass of Character base class

---
+++ rpg_oop/Player.swift
@@ -0,0 +1,30 @@
+//
+//  Player.swift
+//  rpg_oop
+//
+//  Created by William L. Marr III on 3/9/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+class Player: Character {
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        _name = name
+    }
+}
