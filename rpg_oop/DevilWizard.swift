3d408558b41281ca0e878ba372a01dcf011fb1c7
William L. Marr III <hobbyist@wlmiii.net>
2016-03-09 09:49:40 -0500

    Add DevilWizard subclass of Enemy subclass
    Note that the Enemy class is a subclass of the Character base class

---
+++ rpg_oop/DevilWizard.swift
@@ -0,0 +1,19 @@
+//
+//  DevilWizard.swift
+//  rpg_oop
+//
+//  Created by William L. Marr III on 3/9/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+class DevilWizard: Enemy {
+    override var loot: [String] {
+        return ["Magic Wand", "Dark Amulet", "Salted Pork"]
+    }
+    
+    override var type: String {
+        return "Devil Wizard"
+    }
+}
