dba109bcf35450108f07a857d10d99657986c749
William L. Marr III <hobbyist@wlmiii.net>
2016-03-09 09:45:48 -0500

    Add Kimara subclass of Enemy subclass
    Note that the Enemy class is a subclass of the Character base class

---
+++ rpg_oop/Kimara.swift
@@ -0,0 +1,30 @@
+//
+//  Kimara.swift
+//  rpg_oop
+//
+//  Created by William L. Marr III on 3/9/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr)
+        } else {
+            return false
+        }
+    }
+}
